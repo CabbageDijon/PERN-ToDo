231fc9d9fa8ca84fbde0ebb95760af4e0ae03e0c
CDTea <bowntema@gmail.com>
2026-07-16 20:54:17 +0200

    feat: set up Express server and add todos route for CRUD operations

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,24 @@
 import express from "express";
+import cors from "cors";
+import todoRoutes from "./routes/todos.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
+app.use(express.json());
 
-app.get("/about", (req, res) => {
-  res.send("Goodbye World!");
-});
+app.use("/todos", todoRoutes);
+
+//app.get("/", (req, res) => {
+// res.send("Hello World!");
+// });
+
+// app.get("/about", (req, res) => {
+//  res.send("Goodbye World!");
+// });
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
 
-// req = request , res = response
+// req = request from client , res = response from server
